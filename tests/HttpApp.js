c71a18b8507b67d23753d3482791db0ad9387619
Borislav Peev <borislav.asdf@gmail.com>
2014-09-20 15:45:03 +0300

    add more test cases

--- tests/HttpApp.js
+++ tests/HttpApp.js
@@ -39,23 +39,31 @@ UnitestA( 'Parallel domain handling', function ( test ) {
 				} );
 			}, 50 );	
 		}
+		else if ( nreq === 3 ) {
+			throw new Error( '3' );
+		}
 	};
 	app1.onError = function ( err, ctx ) {
 		++nerr;
 		ctx.res.end();
 		if ( nerr === 1 ) {
-			test( err.message === '2' );
-			test( ctx.req.content.toString() === '222' );
-		}
-		else if ( nerr === 2 ) {
-			test( err.message === '1' );
-			test( ctx.req.content.toString() === '111' );
+			test( err.message === '3' );
+			test( ctx.req.content.toString() === '333' );
 			this.cleanup( function () {
 				test.out();
 			} );
+		}
+		else if ( nerr === 2 ) {
+			test( err.message === '2' );
+			test( ctx.req.content.toString() === '222' );
+		}
+		else if ( nerr === 3 ) {
+			test( err.message === '1' );
+			test( ctx.req.content.toString() === '111' );
 		}
 	};
 	app1.startListening();
 	(new HttpRequest( 'http://127.0.0.1:55555' )).send( '111' );
 	(new HttpRequest( 'http://127.0.0.1:55555' )).send( '222' );
+	(new HttpRequest( 'http://127.0.0.1:55555' )).send( '333' );
 } );
